b37b577eb0ada2e62c0f47950aeece74faa1745c
limslmao <97270285+limslmao@users.noreply.github.com>
2026-04-16 22:32:12 +0800

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        sArr = [0] * 26
+        tArr = [0] * 26
+
+        for charS, charT in zip(s, t):
+            sIndex = ord(charS) - ord('a')
+            sArr[sIndex] += 1
+            tIndex = ord(charT) - ord('a')
+            tArr[tIndex] += 1
+
+        return sArr == tArr
